ab407fb1ebcb005b57550cd2313c4628759ea257
theslof <theslof@gmail.com>
2018-02-13 09:18:35 +0100

    Added Firebase

--- app/src/main/java/se/newton/chatapp/MainActivity.java
+++ app/src/main/java/se/newton/chatapp/MainActivity.java
@@ -3,11 +3,25 @@ package se.newton.chatapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        //Do stuff
     }
 }
