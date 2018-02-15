a601dc046d1e40b3bc4f0433b85dc4e213a00411
oretx <dragomir.d53@gmail.com>
2018-02-15 14:10:55 +0100

    Added sidedrawer to project!

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -52,45 +52,46 @@ public final class R {
         public static final int googleg_standard_color_18 = 0x7f070081;
     }
     public static final class id {
-        public static final int adjust_height = 0x7f08001b;
-        public static final int adjust_width = 0x7f08001c;
-        public static final int auto = 0x7f080021;
-        public static final int center = 0x7f08002d;
-        public static final int dark = 0x7f08003f;
-        public static final int email = 0x7f08004b;
-        public static final int icon_only = 0x7f080063;
-        public static final int light = 0x7f08006b;
-        public static final int none = 0x7f08007b;
-        public static final int normal = 0x7f08007c;
-        public static final int radio = 0x7f080091;
-        public static final int standard = 0x7f0800bf;
-        public static final int text = 0x7f0800c6;
-        public static final int text2 = 0x7f0800c7;
-        public static final int wide = 0x7f0800e4;
-        public static final int wrap_content = 0x7f0800e7;
+        public static final int adjust_height = 0x7f08001c;
+        public static final int adjust_width = 0x7f08001d;
+        public static final int auto = 0x7f080022;
+        public static final int center = 0x7f08002e;
+        public static final int dark = 0x7f080040;
+        public static final int email = 0x7f08004d;
+        public static final int icon_only = 0x7f080065;
+        public static final int light = 0x7f08006e;
+        public static final int none = 0x7f080085;
+        public static final int normal = 0x7f080086;
+        public static final int radio = 0x7f08009b;
+        public static final int standard = 0x7f0800c9;
+        public static final int text = 0x7f0800d0;
+        public static final int text2 = 0x7f0800d1;
+        public static final int toolbar = 0x7f0800dd;
+        public static final int wide = 0x7f0800f0;
+        public static final int wrap_content = 0x7f0800f3;
     }
     public static final class integer {
         public static final int google_play_services_version = 0x7f090008;
     }
     public static final class string {
-        public static final int common_google_play_services_enable_button = 0x7f0d0024;
-        public static final int common_google_play_services_enable_text = 0x7f0d0025;
-        public static final int common_google_play_services_enable_title = 0x7f0d0026;
-        public static final int common_google_play_services_install_button = 0x7f0d0027;
-        public static final int common_google_play_services_install_text = 0x7f0d0028;
-        public static final int common_google_play_services_install_title = 0x7f0d0029;
-        public static final int common_google_play_services_notification_channel_name = 0x7f0d002a;
-        public static final int common_google_play_services_notification_ticker = 0x7f0d002b;
-        public static final int common_google_play_services_unknown_issue = 0x7f0d002c;
-        public static final int common_google_play_services_unsupported_text = 0x7f0d002d;
-        public static final int common_google_play_services_update_button = 0x7f0d002e;
-        public static final int common_google_play_services_update_text = 0x7f0d002f;
-        public static final int common_google_play_services_update_title = 0x7f0d0030;
-        public static final int common_google_play_services_updating_text = 0x7f0d0031;
-        public static final int common_google_play_services_wear_update_text = 0x7f0d0032;
-        public static final int common_open_on_phone = 0x7f0d0033;
-        public static final int common_signin_button_text = 0x7f0d0034;
-        public static final int common_signin_button_text_long = 0x7f0d0035;
+        public static final int common_google_play_services_enable_button = 0x7f0e0025;
+        public static final int common_google_play_services_enable_text = 0x7f0e0026;
+        public static final int common_google_play_services_enable_title = 0x7f0e0027;
+        public static final int common_google_play_services_install_button = 0x7f0e0028;
+        public static final int common_google_play_services_install_text = 0x7f0e0029;
+        public static final int common_google_play_services_install_title = 0x7f0e002a;
+        public static final int common_google_play_services_notification_channel_name = 0x7f0e002b;
+        public static final int common_google_play_services_notification_ticker = 0x7f0e002c;
+        public static final int common_google_play_services_unknown_issue = 0x7f0e002d;
+        public static final int common_google_play_services_unsupported_text = 0x7f0e002e;
+        public static final int common_google_play_services_update_button = 0x7f0e002f;
+        public static final int common_google_play_services_update_text = 0x7f0e0030;
+        public static final int common_google_play_services_update_title = 0x7f0e0031;
+        public static final int common_google_play_services_updating_text = 0x7f0e0032;
+        public static final int common_google_play_services_wear_update_text = 0x7f0e0033;
+        public static final int common_open_on_phone = 0x7f0e0034;
+        public static final int common_signin_button_text = 0x7f0e0035;
+        public static final int common_signin_button_text_long = 0x7f0e0036;
     }
     public static final class styleable {
         public static final int[] LoadingImageView = { 0x7f030056, 0x7f0300be, 0x7f0300bf };
---
+++ app/src/main/java/se/newton/chatapp/fragment/First.java
@@ -0,0 +1,30 @@
+package se.newton.chatapp.fragment;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import se.newton.chatapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class First extends Fragment {
+
+
+    public First() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+}
