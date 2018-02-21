aa30b4da80380d2c129b20e015eebe468972e4d8
fr9 <eliasf@me.com>
2018-02-21 13:41:52 +0100

    Created ProfileFragment and profile_fragment layout.

--- app/src/main/java/se/newton/chatapp/fragment/ProfileFragment.java
+++ app/src/main/java/se/newton/chatapp/fragment/ProfileFragment.java
@@ -3,18 +3,11 @@ package se.newton.chatapp.fragment;
 import android.app.Fragment;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.RequestManager;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 import se.newton.chatapp.R;
-import se.newton.chatapp.service.Database;
 import se.newton.chatapp.viewmodel.ProfileViewModel;
 import se.newton.chatapp.databinding.ProfileFragmentBinding;
 
@@ -24,18 +17,11 @@ import se.newton.chatapp.databinding.ProfileFragmentBinding;
 
 public class ProfileFragment extends Fragment {
 
-    private String fUserUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-    private ProfileViewModel viewModel;
-    private static final String TAG = "ProfileFragment";
+    private ProfileViewModel viewModel= new ProfileViewModel();
+
 
     public ProfileFragment() {
         // Required empty public constructor
-    }
-
-    public static ProfileFragment newInstance() {
-        Log.d(TAG, "Creating profile fragment");
-        ProfileFragment fragment = new ProfileFragment();
-        return fragment;
     }
 
 
@@ -46,9 +32,6 @@ public class ProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        viewModel = new ProfileViewModel(Glide.with(this), fUserUid);
-
         // Inflate the layout for this fragment
         ProfileFragmentBinding binding = DataBindingUtil.inflate(inflater,R.layout.profile_fragment, container, false);
         binding.setViewModel(viewModel);
--- app/src/main/java/se/newton/chatapp/viewmodel/ProfileViewModel.java
+++ app/src/main/java/se/newton/chatapp/viewmodel/ProfileViewModel.java
@@ -2,18 +2,9 @@ package se.newton.chatapp.viewmodel;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
-import android.databinding.BindingAdapter;
-import android.graphics.Bitmap;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.RequestManager;
 
 import se.newton.chatapp.BR;
-import se.newton.chatapp.R;
 import se.newton.chatapp.model.User;
-import se.newton.chatapp.service.Database;
-import se.newton.chatapp.service.UserManager;
 
 /**
  * Created by fr9b on 2018-02-21.
@@ -21,44 +12,27 @@ import se.newton.chatapp.service.UserManager;
 
 public class ProfileViewModel extends BaseObservable {
     private User user = new User();
-    private String uid;
-    private RequestManager glideManager;
 
 
-    public ProfileViewModel(RequestManager glideManager, String uid) {
-        user = UserManager.getUser(glideManager, uid);
-        this.glideManager = glideManager;
-    }
 
     public void setUser(User user) {
         this.user = user;
-        notifyPropertyChanged(BR.user);
-        //notifyPropertyChanged(BR.userName);
-        //notifyPropertyChanged(BR.profileImage);
-        //notifyPropertyChanged(BR.userBio);
+        notifyPropertyChanged(BR.userName);
+        notifyPropertyChanged(BR.profileImage);
     }
 
     @Bindable
-    public User getUser() {
-        return user;
+    public String getUserName() {
+        return user.getDisplayName();
+    }
+
+    @Bindable
+    public String getProfileImage() {
+        return user.getProfileImage();
     }
 
     @Bindable
     public String getUserBio() {
         return user.getBio();
     }
-
-    @BindingAdapter("android:profile")
-    public static void loadProfileImage(ImageView view, Bitmap image) {
-        if (image == null)
-            view.setImageResource(R.drawable.ic_profile_image_placeholder);
-        else
-            view.setImageBitmap(image);
-    }
-
-    public void startPrivateChannelBtn() {
-        // Start new chat channel with user
-    }
-
 }
-
