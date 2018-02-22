30ada21d5d50d540cb3407b0f61037cc70332ed8
theslof <theslof@gmail.com>
2018-02-22 11:49:21 +0100

    Moved image download from HTTP call to Glide.

--- app/src/main/java/se/newton/chatapp/service/UserManager.java
+++ app/src/main/java/se/newton/chatapp/service/UserManager.java
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,49 +13,39 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 import se.newton.chatapp.model.User;
 
 public class UserManager {
     private static Map<String, User> users = new HashMap<>();
 
-    private UserManager(){}
+    private UserManager() {
+    }
 
-    public static User getUser(String uid){
-        if(!users.containsKey(uid))
-            return createUser(uid);
+    public static User getUser(RequestManager glideManager, String uid) {
+        if (!users.containsKey(uid))
+            return createUser(glideManager, uid);
         return users.get(uid);
     }
 
-    private static User createUser(String uid){
+    private static User createUser(RequestManager glideManager, String uid) {
         User user = new User(uid);
         users.put(uid, user);
         Database.getUser(uid, u -> {
             user.setDisplayName(u.getDisplayName());
             user.setProfileImage(u.getProfileImage());
-            GetBitmapFromURLAsync getBitmapFromURLAsync = new GetBitmapFromURLAsync();
-            getBitmapFromURLAsync.execute(user);
+
+            new Thread(() -> {
+                try {
+                    user.setProfileBitmap(glideManager.load(u.getProfileImage()).asBitmap().into(-1, -1).get());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }).start();
         });
         return user;
-    }
-
-    private static class GetBitmapFromURLAsync extends AsyncTask<User, Void, Bitmap> {
-        @Override
-        protected Bitmap doInBackground(User... params) {
-            try {
-                User user = params[0];
-                URL url = new URL(user.getProfileImage());
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setDoInput(true);
-                connection.connect();
-                InputStream input = connection.getInputStream();
-                Bitmap myBitmap = BitmapFactory.decodeStream(input);
-                user.setProfileBitmap(myBitmap);
-                return myBitmap;
-            } catch (IOException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
     }
 }
