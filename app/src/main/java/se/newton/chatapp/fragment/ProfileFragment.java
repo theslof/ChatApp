8c5c334d7de0fa5da14eb302b4283a2cbd7e6081
fr9 <eliasf@me.com>
2018-02-22 13:12:18 +0100

    Added profile to sideDrawer.

--- app/src/main/java/se/newton/chatapp/fragment/ProfileFragment.java
+++ app/src/main/java/se/newton/chatapp/fragment/ProfileFragment.java
@@ -3,6 +3,7 @@ package se.newton.chatapp.fragment;
 import android.app.Fragment;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import se.newton.chatapp.R;
+import se.newton.chatapp.service.Database;
 import se.newton.chatapp.viewmodel.ProfileViewModel;
 import se.newton.chatapp.databinding.ProfileFragmentBinding;
 
@@ -20,13 +22,18 @@ import se.newton.chatapp.databinding.ProfileFragmentBinding;
 
 public class ProfileFragment extends Fragment {
 
-    // Get current user;
     private String fUserUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-
     private ProfileViewModel viewModel= new ProfileViewModel(fUserUid);
+    private static final String TAG = "ProfileFragment";
 
     public ProfileFragment() {
         // Required empty public constructor
+    }
+
+    public static ProfileFragment newInstance() {
+        Log.d(TAG, "Creating profile fragment");
+        ProfileFragment fragment = new ProfileFragment();
+        return fragment;
     }
 
 
