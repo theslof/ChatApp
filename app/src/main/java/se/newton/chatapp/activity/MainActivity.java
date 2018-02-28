84104a4e3dc39d8dd58a05477c3b4d1147966514
fr9 <eliasf@me.com>
2018-02-28 13:29:37 +0100

    Profile and settings to side drawer

--- app/src/main/java/se/newton/chatapp/activity/MainActivity.java
+++ app/src/main/java/se/newton/chatapp/activity/MainActivity.java
@@ -180,14 +180,12 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_channel_one) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_my_profile) {
             ProfileFragment profileFragment = ProfileFragment.newInstance(fUser.getUid());
 
             getFragmentManager().beginTransaction()
@@ -195,7 +193,7 @@ public class MainActivity extends AppCompatActivity
                     .addToBackStack(fUser.getUid())
                     .commit();
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
