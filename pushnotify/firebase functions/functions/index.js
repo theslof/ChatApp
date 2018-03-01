4e1d290a26d3aae121425cc1ce60d185eb2d7870
Martin Blomberg <martin.blomberg@outlook.com>
2018-03-01 17:34:21 +0100

    Updated method in index.js

--- pushnotify/firebase functions/functions/index.js
+++ pushnotify/firebase functions/functions/index.js
@@ -1,5 +1,11 @@
 const functions = require('firebase-functions');
 
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+// Get the Messaging service for the default app
+var defaultMessaging = admin.messaging();
+
 //Trigger a function when a new document is created in messages
 exports.createmessage = functions.firestore
   .document('messages/{documentId}')
@@ -12,6 +18,24 @@ exports.createmessage = functions.firestore
     var cid = newValue.cid;
 
     // perform desired operations ...
+    // Send Push Message
+
+    // Send a message
+    var message = {
+      notification: {
+        title: 'Test title',
+        body: 'Test body'
+      },
+      topic: 'default'};
+
+   return defaultMessaging.send(message)
+      .then((response) => {
+        // Response is a message ID string.
+        console.log('Successfully sent message:', response);
+      })
+      .catch((error) => {
+        console.log('Error sending message:', error);
+      });
 });
 
 //https://firebase.google.com/docs/functions/get-started?authuser=0
