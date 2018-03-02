f6a792a3773f3a4c7cfdcd6314988e131d5d2634
Martin Blomberg <martin.blomberg@outlook.com>
2018-03-02 16:26:05 +0100

    Added javadoc comments.

--- app/src/main/java/se/newton/chatapp/pushnotify/Messaging.java
+++ app/src/main/java/se/newton/chatapp/pushnotify/Messaging.java
@@ -15,10 +15,19 @@ public final class Messaging {
         //ToDo: Register a notification channel for >Oreo. Set default channel in manifest.
     }
 
+    /**
+     * Subscribes for push messages when new messages are available.
+     * Will display a notification if app isn't running in foreground.
+     * @param  cid channel name to subscribe to
+     */
     public static void subscribeToTopic(String cid) {
         FirebaseMessaging.getInstance().subscribeToTopic(cid);
     }
 
+    /**
+     * Unsubscribes from receiving push messages-
+     * @param  cid channel name to unsubscribe from
+     */
     public static void unsubscribeToTopic(String cid) {
         FirebaseMessaging.getInstance().unsubscribeFromTopic(cid);
     }
